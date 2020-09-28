00ffc7e067521b4b21774b5817dcddf072d72215
sanjeevbaurai <sanjeev.baurai@gmail.com>
2020-09-29 00:11:10 +0100

    Updates

---
+++ precache-manifest.6eb48923874eb0013c7ee4c9832bc100.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "096ab213acdbc43111fb47d393d3c73e",
+    "url": "/sanjeevbaurai/star-war-heros-app/index.html"
+  },
+  {
+    "revision": "63267d506aafe4213096",
+    "url": "/sanjeevbaurai/star-war-heros-app/static/css/main.d87d0bc6.chunk.css"
+  },
+  {
+    "revision": "04c7d4e025e972cc28f7",
+    "url": "/sanjeevbaurai/star-war-heros-app/static/js/2.36791b64.chunk.js"
+  },
+  {
+    "revision": "98b53ee81b497d75f7c86a93c82f9a7a",
+    "url": "/sanjeevbaurai/star-war-heros-app/static/js/2.36791b64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63267d506aafe4213096",
+    "url": "/sanjeevbaurai/star-war-heros-app/static/js/main.9be78e86.chunk.js"
+  },
+  {
+    "revision": "cfe1b10f9bef24e37b3c",
+    "url": "/sanjeevbaurai/star-war-heros-app/static/js/runtime-main.598ff3b8.js"
+  }
+]);
